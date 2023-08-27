04ebd943017f1ce804141875310ad71f8093b40e
Prathwik <64214685+prathwik0@users.noreply.github.com>
2023-08-28 00:44:24 +0530

    fix: Linking issues in pre-rendered content

--- src/routes/Positioning/+layout.svelte
+++ src/routes/Positioning/+layout.svelte
@@ -1,5 +1,15 @@
-<script>
+<script lang="ts">
   import "./style.css";
+  import { base } from "$app/paths";
 </script>
+
+<nav>
+  <a href="{base}/">Back</a>
+  <a href="{base}/positioning/static">Static</a>
+  <a href="{base}/positioning/relative">Relative</a>
+  <a href="{base}/positioning/absolute">Absolute</a>
+  <a href="{base}/positioning/fixed">Fixed</a>
+  <a href="{base}/positioning/sticky">Sticky</a>
+</nav>
 
 <slot />
--- src/routes/Positioning/+page.svelte
+++
@@ -1,8 +0,0 @@
-<nav>
-  <a href="/">Back</a>
-  <a href="positioning/static">Static</a>
-  <a href="positioning/relative">Relative</a>
-  <a href="positioning/absolute">Absolute</a>
-  <a href="positioning/fixed">Fixed</a>
-  <a href="positioning/sticky">Sticky</a>
-</nav>
