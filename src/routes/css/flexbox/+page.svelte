d0a8a9f37987bd929756fa5eb00e5c7f40fe8ca8
Prathwik <64214685+prathwik0@users.noreply.github.com>
2023-09-02 04:29:13 +0530

    Iteration 1 of CSS Interactive GUI
    At this point, my goal was only to make a flexbox with elements inside it. I didn't aim to make a recursive box.

--- src/routes/css/flexbox/+page.svelte
+++ src/routes/css/flexbox/+page.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+    import SelectCSSProperty from './SelectCSSProperty.svelte';
+
     let container = {
         display: 'flex',
         'flex-direction': {
@@ -6,20 +8,20 @@
             options: ['row', 'row-reverse', 'column', 'column-reverse'],
         },
         'flex-wrap': {
-            value: 'wrap',
-            options: ['nowrap', 'wrap', 'wrap-reverse'],
+            value: 'nowrap',
+            options: ['wrap', 'nowrap', 'wrap-reverse'],
         },
         'justify-content': {
-            value: 'space-between',
+            value: 'flex-start',
             options: ['flex-start', 'flex-end', 'center', 'space-between', 'space-around', 'space-evenly', 'start', 'end', 'left', 'right'],
         },
         'align-items': {
-            value: 'center',
-            options: ['stretch', 'center', 'flex-start', 'flex-end', 'baseline', 'first baseline', 'last baseline', 'start', 'end', 'self-start', 'self-end', 'safe center', 'unsafe center'],
+            value: 'flex-start',
+            options: ['flex-start', 'flex-end', 'center', 'stretch', 'baseline', 'first baseline', 'last baseline', 'start', 'end', 'self-start', 'self-end', 'safe center', 'unsafe center'],
         },
         'align-content': {
-            value: 'center',
-            options: ['stretch', 'center', 'flex-start', 'flex-end', 'space-between', 'space-around', 'space-evenly', 'start', 'end', 'baseline', 'first baseline', 'last baseline', 'safe center', 'unsafe center'],
+            value: 'flex-start',
+            options: ['flex-start', 'flex-end', 'center', 'stretch', 'space-between', 'space-around', 'space-evenly', 'start', 'end', 'baseline', 'first baseline', 'last baseline', 'safe center', 'unsafe center'],
         },
         width: {
             value: 50,
@@ -35,89 +37,65 @@
         // border: '10px solid rgb(233, 156, 156)',
         // margin: '10%',
     };
+
+    let item = {
+        'flex-grow': 0,
+        'flex-shrink': 1,
+        'flex-basis': {
+            value: 'auto',
+            options: ['%'],
+        },
+        'align-self': {
+            value: 'auto',
+            options: ['auto', 'flex-start', 'flex-end', 'center', 'stretch', 'baseline'], //'first baseline', 'last baseline', 'start', 'end', 'self-start', 'self-end', 'safe center', 'unsafe center'],
+        },
+        order: 0,
+    };
+
+    let count = 0;
 </script>
 
-<label>
-    <p>Choose Width : {container.width.value + container.width.unit}</p>
-    <input type="number" bind:value={container.width.value} min="0" max="100" class="inline-block p-2 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 sm:text-xs focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
-    <input type="range" bind:value={container.width.value} min="0" max="100" />
-    <select bind:value={container.width.unit} class="inline-block p-2 mb-6 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-        {#each container.width.options as option}
-            <option value={option}>
-                {option}
-            </option>
-        {/each}
-    </select>
-</label>
+<div class="flex justify-center p-3">
+    <span>Number of Items : </span>
+    <input type="number" bind:value={count} min="0" max="100" class="inline-block p-1 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
+</div>
 
-<label>
-    <p>Choose Height : {container.height.value + container.height.unit}</p>
-    <input type="number" bind:value={container.height.value} min="0" max="100" class="inline-block p-2 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 sm:text-xs focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
-    <input type="range" bind:value={container.height.value} min="0" max="100" />
-    <select bind:value={container.height.unit} class="inline-block p-2 mb-6 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-        {#each container.height.options as option}
-            <option value={option}>
-                {option}
-            </option>
-        {/each}
-    </select>
-</label>
+<div class="flex flex-wrap">
+    <div class="w-1/3 inline-grid grid-cols-1">
+        <label>
+            <p class="text-xs max-w-fit sm:text-sm">Choose Width : {container.width.value + container.width.unit}</p>
+            <input type="number" bind:value={container.width.value} min="0" max="100" class="inline-block p-2 text-xs sm:text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
+            <input type="range" bind:value={container.width.value} min="0" max="100" />
+            <select bind:value={container.width.unit} class="inline-block w-full max-w-fit max p-2 mb-1 text-xs sm:text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                {#each container.width.options as option}
+                    <option value={option}>
+                        {option}
+                    </option>
+                {/each}
+            </select>
+        </label>
 
-<div class="grid grid-cols-3">
-    <label>
-        <p>flex-direction</p>
-        <select bind:value={container['flex-direction'].value} class="inline-block p-2 mb-6 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-            {#each container['flex-direction'].options as option}
-                <option value={option}>
-                    {option}
-                </option>
-            {/each}
-        </select>
-    </label>
+        <label>
+            <p class="text-xs max-w-fit sm:text-sm">Choose Height : {container.height.value + container.height.unit}</p>
+            <input type="number" bind:value={container.height.value} min="0" max="100" class="inline-block p-2 text-xs sm:text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
+            <input type="range" bind:value={container.height.value} min="0" max="100" />
+            <select bind:value={container.height.unit} class="inline-block w-full max-w-fit max p-2 mb-1 text-xs sm:text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                {#each container.height.options as option}
+                    <option value={option}>
+                        {option}
+                    </option>
+                {/each}
+            </select>
+        </label>
+    </div>
 
-    <label>
-        <p>flex-wrap</p>
-        <select bind:value={container['flex-wrap'].value} class="inline-block p-2 mb-6 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-            {#each container['flex-wrap'].options as option}
-                <option value={option}>
-                    {option}
-                </option>
-            {/each}
-        </select>
-    </label>
-
-    <label>
-        <p>justify-content</p>
-        <select bind:value={container['justify-content'].value} class="inline-block p-2 mb-6 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-            {#each container['justify-content'].options as option}
-                <option value={option}>
-                    {option}
-                </option>
-            {/each}
-        </select>
-    </label>
-
-    <label>
-        <p>align-items</p>
-        <select bind:value={container['align-items'].value} class="inline-block p-2 mb-6 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-            {#each container['align-items'].options as option}
-                <option value={option}>
-                    {option}
-                </option>
-            {/each}
-        </select>
-    </label>
-
-    <label>
-        <p>align-content</p>
-        <select bind:value={container['align-content'].value} class="inline-block p-2 mb-6 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-            {#each container['align-content'].options as option}
-                <option value={option}>
-                    {option}
-                </option>
-            {/each}
-        </select>
-    </label>
+    <div class="inline-grid w-1/3 grid-cols-3">
+        <SelectCSSProperty property="flex-direction" bind:info={container['flex-direction']} />
+        <SelectCSSProperty property="flex-wrap" bind:info={container['flex-wrap']} />
+        <SelectCSSProperty property="justify-content" bind:info={container['justify-content']} />
+        <SelectCSSProperty property="align-items" bind:info={container['align-items']} />
+        <SelectCSSProperty property="align-content" bind:info={container['align-content']} />
+    </div>
 </div>
 
 <div
@@ -131,20 +109,19 @@
             width: {container.width.value + container.width.unit};
             height: {container.height.value + container.height.unit}"
 >
-    <div class="item">asdfasdfasdf1</div>
-    <div class="item">asdf2</div>
-    <div class="item">asdf3</div>
+    {#each { length: count } as _, i}
+        <div class="item">asdf {i}</div>
+    {/each}
 </div>
 
-<style>
+<style lang="postcss">
     .container {
-        background-color: #ea6363;
         margin: 10px;
-        border: 10px solid rgb(233, 156, 156);
+        @apply border-2 border-black dark:border-white bg-gray-300 dark:bg-neutral-900;
         /*  padding: 10%; */
     }
     .item {
-        border: 2px solid black;
+        @apply border-2 border-black dark:border-white;
         padding: 10px;
     }
 </style>
---
+++ src/routes/css/flexbox/SelectCSSProperty.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+    export let property: string;
+    export let info: {
+        value: string;
+        options: string[];
+    };
+</script>
+
+<label class="flex flex-col w-full p-1">
+    <p class="text-xs max-w-fit sm:text-sm">{property}</p>
+    <select bind:value={info.value} class="inline-block w-full max-w-fit max p-2 mb-1 text-xs sm:text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+        {#each info.options as option}
+            <option value={option}>
+                {option}
+            </option>
+        {/each}
+    </select>
+</label>
